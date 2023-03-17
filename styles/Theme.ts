3fd4f5d007851b8c71b758959fa3e1ca87ec9619
kimgyuha <kimgyuha25@gmail.com>
2023-03-17 11:26:27 +0900

    🎉 :: 프로젝트 세팅
    기본 color theme 설정

---
+++ styles/Theme.ts
@@ -0,0 +1,12 @@
+export const Theme = {
+  ThePurple: '#8066F5',
+  White: '#ffffff',
+  Black: '#000000',
+  Red: '#DA5858',
+  Gray: {
+    75: '#BFBFBF',
+    50: '#808080',
+    25: '#404040',
+    5: '#F2F2F2',
+  },
+};
