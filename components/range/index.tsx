393ca8646b67ddd06472f5eef6f375fdac26a2d5
김상구 <spair0039@gmail.com>
2023-03-19 21:48:11 +0900

    ✨: 기본 구조 완성

--- components/range/index.tsx
+++ components/range/index.tsx
@@ -7,11 +7,15 @@ type mainPropsType ={
     max: number;
     step: number;
     width: string;
+    label: string;
 }
 
-function Range({onChange, value, min, max, step, width}:mainPropsType){
+function Range({onChange, value, min, max, step, width, label}:mainPropsType){
     return(
         <div>
+            <div>
+            {label}{value}
+            </div>
             <input onChange={onChange} type="range" value={value} min={min} max={max} step={step} />
         </div>
     )
