be44a735b4f5a6a4cede645b04c77a6cfc2cc2df
Jin hyeon <kangjin627@gmail.com>
2023-03-26 21:42:20 +0900

    🎨:: 오타 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-<<<<<<< HEAD
-import Header from '@/components/header/header'
-
-=======
->>>>>>> develop
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
