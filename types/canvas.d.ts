3a3fe3aa427295b98764a497f65baeb37c9a4e66
kimgyuha <kimgyuha25@gmail.com>
2023-03-27 22:00:35 +0900

    ✨:: canvas 타입 설정

--- types/canvas.d.ts
+++ types/canvas.d.ts
@@ -1,6 +1,7 @@
 declare type CanvasOptions = {
   color: string;
   tool: boolean;
+  backgroundColor: string;
 };
 
 declare type CanvasUseState = {
@@ -21,4 +22,14 @@ declare type CanvasToolUseState = {
 declare type ToolSize = {
   brush: number;
   eraser: number;
+};
+
+declare type ToolSizeUseState = {
+  toolWidth: ToolSize;
+  setToolWidth: React.Dispatch<React.SetStateAction<ToolSize>>;
+};
+
+declare type CanvasSize = {
+  width: number;
+  height: number;
 };
