d580f69f22b3df81d5393720a1fb7f9ba06a3963
한예슬 <yangsooh386@gmail.com>
2023-03-28 23:24:57 +0900

    🎨:: emailCheck page 수정

--- pages/auth/emailCheck.tsx
+++ pages/auth/emailCheck.tsx
@@ -1,20 +1,11 @@
-import AuthBox from "@/components/common/authBox";
-import { Button } from "@/components/common/button/style";
-import Input from "@/components/common/input";
+import EmailCheckC from "@/components/auth/emailCheckC";
 import styled from "@emotion/styled";
-import { useState } from "react";
 
 const EmailCheck = (): JSX.Element => {
-    const [checkNumber, setCheckNumber] = useState<string>('');
 
     return (
       <Contanier>
-        <AuthBox title="이메일 주소 인증" sub="입력한 이메일 주소로 전송된 6자리 인증번호를 입력하세요. 인증번호는 5분 뒤에 만료됩니다.">
-          <Content>
-            <Input value={checkNumber} setValue={setCheckNumber} title="인증번호" />
-            <Button MainColor>다음</Button>
-          </Content>
-        </AuthBox>
+        <EmailCheckC />
       </Contanier>
     );
 };
@@ -28,8 +19,3 @@ const Contanier = styled.div`
     align-items: center;
 `;
 
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
