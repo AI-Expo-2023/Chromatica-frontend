c2d5ff467c25bb8c9b0572ed563c141b3d27e060
한예슬 <yangsooh386@gmail.com>
2023-03-28 23:25:10 +0900

    🎨:: completeEmail page 수정

--- pages/auth/completeEmail.tsx
+++ pages/auth/completeEmail.tsx
@@ -1,16 +1,10 @@
-import AuthBox from "@/components/common/authBox";
-import { Button } from "@/components/common/button/style";
+import CompleteEmailC from "@/components/auth/completeEmailC";
 import styled from "@emotion/styled";
 
 const CompleteEmail = (): JSX.Element => {
     return (
       <Contanier>
-        <AuthBox title="비밀번호 변경" sub="비밀번호가 변경되었습니다">
-          <Content>
-            <Button MainColor>로그인</Button>
-            <Button White>닫기</Button>
-          </Content>
-        </AuthBox>
+        <CompleteEmailC />
       </Contanier>
     );
 }
@@ -23,9 +17,3 @@ const Contanier = styled.div`
     justify-content: center;
     align-items: center;
 `
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-`;
