f137e46b7bc85a36f834bc7c8ce9730eed3781ff
한예슬 <yangsooh386@gmail.com>
2023-03-28 23:23:58 +0900

    🎨:: receiveEmail page 수정

--- pages/auth/receiveEmail.tsx
+++ pages/auth/receiveEmail.tsx
@@ -1,20 +1,10 @@
-import AuthBox from '@/components/common/authBox';
-import { Button } from '@/components/common/button/style';
-import Input from '@/components/common/input';
+import RecevieEmailC from '@/components/auth/recevieEmailC';
 import styled from '@emotion/styled';
-import { useState } from 'react';
 
 const ReceiveEmail = (): JSX.Element => {
-  const [email, setEmail] = useState<string>('');
-
   return (
     <Contanier>
-      <AuthBox title="비밀번호 변경" sub="비밀번호를 변경하려면 이메일 인증이 필요합니다">
-        <Content>
-          <Input value={email} setValue={setEmail} title="이메일" />
-          <Button MainColor>다음</Button>
-        </Content>
-      </AuthBox>
+      <RecevieEmailC />
     </Contanier>
   );
 };
@@ -26,10 +16,4 @@ const Contanier = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
 `;
