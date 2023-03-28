df454916cc2208716bfbce02019528f422333b04
김상구 <spair0039@gmail.com>
2023-03-28 17:25:39 +0900

    💄: gap 추가

--- components/post/style.ts
+++ components/post/style.ts
@@ -5,5 +5,6 @@ import styled from "@emotion/styled";
 export const tagOptionDiv = styled.div`
     display: flex;
     padding: 8px;
+    gap: 4px;
     border-bottom: 1px solid ${Theme.Gray[75]};
 `
