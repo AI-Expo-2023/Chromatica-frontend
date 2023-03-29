98199a61b55a18ed0e9c0ab8b3d81d26fe552d47
kimgyuha <kimgyuha25@gmail.com>
2023-03-29 22:40:53 +0900

    :lipstick: :: canvas style파일 추가

---
+++ components/img2img/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+`;
+export const Canvas = styled.canvas`
+  border-radius: 8px;
+  box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.25);
+`;
