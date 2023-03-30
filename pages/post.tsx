6745b57ec86ff8503ef9e0b6f90a05461cf5d5ff
김상구 <spair0039@gmail.com>
2023-03-30 18:53:23 +0900

    💄: 태그 선택기 디자인수정

--- pages/post.tsx
+++ pages/post.tsx
@@ -5,6 +5,8 @@ import { Button } from "@/components/common/button/style";
 import { Add20Filled } from "@fluentui/react-icons";
 import { Theme } from "@/styles/theme/Theme";
 import { TextArea } from "@/components/post/textArea";
+import { RemovableTag } from "@/components/post/tagRemovable";
+import styled from "@emotion/styled";
 
 type postType = {
     photo: string;
@@ -26,13 +28,20 @@ export default function PostPage(){
             <img src="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/6152bc164687275.63fb405a39dda.jpg" alt="업로드하려는 이미지 미리 보기" width='800' height='500' />
             <Input value={title} setValue={setTitle} title='제목' width="100%" />
             <TextArea value={Desc} setValue={setDesc} title='설명' width="100%"/>
-            <div>
+            <Horizonal>
                 <Button onClick={()=>setTSstatus(!isTSenabled)}><Add20Filled primaryFill={Theme.Black}/>태그 추가</Button>
                 {isTSenabled ? <TagSelector /> : null}
                 {TagList.map((data)=>
-                    {return <span key={data}>{data}</span>;
+                    {return <RemovableTag data={data}/>;
                 })}
-            </div>
+            </Horizonal>
         </>
     )
 }
+
+const Horizonal = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    align-items: center;
+`
