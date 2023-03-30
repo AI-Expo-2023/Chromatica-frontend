f71b0fe29581391261f53039da928eb07b51f122
김상구 <spair0039@gmail.com>
2023-03-30 18:48:35 +0900

    💄: 태그 디자인

--- components/post/tagRemovable.tsx
+++ components/post/tagRemovable.tsx
@@ -1,4 +1,5 @@
 import { Theme } from "@/styles/theme/Theme"
+import styled from "@emotion/styled";
 import { Dismiss20Filled } from "@fluentui/react-icons"
 
 type tagType = {
@@ -8,12 +9,24 @@ type tagType = {
 
 export const RemovableTag = ({data, onClick}:tagType)=>{
     return(
-        <div>
+        <TagContainer>
             <button>
-                <Dismiss20Filled primaryFill={Theme.Black} />
+                <Dismiss20Filled primaryFill={Theme.Gray[50]} />
             </button>
             {data}
-        </div>
+        </TagContainer>
     )
 }
 
+const TagContainer = styled.div`
+    border: 1px ${Theme.Gray[75]} solid;
+    border-radius: 888px;
+    padding: 6px 12px;
+    display: flex;
+    align-items: center;
+
+    & > button{
+        background: none;
+        border: none;
+    }
+`
