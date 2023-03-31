289fa9e49df11cf5eb18ef69f70e9c3b5b0e0e7d
김상구 <spair0039@gmail.com>
2023-03-31 11:57:47 +0900

    ✨: 이미지 src 상태로 전환

--- pages/post.tsx
+++ pages/post.tsx
@@ -18,14 +18,14 @@ type postType = {
 export default function PostPage(){
     const [title, setTitle] = useState<string>('');
     const [Desc, setDesc] = useState<string>('');
-    const [Photo, setPhoto] = useState<string>('');
+    const [Photo, setPhoto] = useState<string>('https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/6152bc164687275.63fb405a39dda.jpg');
     const [TagList, setTagList] = useState<string[]>(["띵명작","자연","사이버펑크","현대적","도시"]);
 
     const [isTSenabled, setTSstatus] = useState<boolean>(false);
 
     return(
         <>
-            <PostPreview src="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/6152bc164687275.63fb405a39dda.jpg" alt="업로드하려는 이미지 미리 보기" />
+            <PostPreview src={Photo} alt="업로드하려는 이미지 미리 보기" />
             <Input value={title} setValue={setTitle} title='제목' width="100%" />
             <TextArea value={Desc} setValue={setDesc} title='설명' width="100%"/>
             <Horizonal>
