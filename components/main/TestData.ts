5c5de3ba56cfa5a61587782cc691d0dd468a3331
Im Tae Gon <103089628+eternrust@users.noreply.github.com>
2023-03-31 10:05:22 +0900

    Update TestData.ts

--- components/main/TestData.ts
+++ components/main/TestData.ts
@@ -7,47 +7,4 @@ export interface RankProps {
   rank?: number;
 }
 
-export const Data: RankProps[] = [{
-  photoID: 1,
-  photo: 'abc',
-  head: '흔한 제목',
-  name: '그저 이름',
-  like: 123,
-  rank: 1
-}, {
-  photoID: 2,
-  photo: 'abc',
-  head: '흔한 제목',
-  name: '그저 이름',
-  like: 123,
-  rank: 2
-}, {
-  photoID: 3,
-  photo: 'abc',
-  head: '흔한 제목',
-  name: '그저 이름',
-  like: 123,
-  rank: 3
-}, {
-  photoID: 4,
-  photo: 'abc',
-  head: '흔한 제목',
-  name: '그저 이름',
-  like: 123,
-  rank: 4
-}, {
-  photoID: 5,
-  photo: 'abc',
-  head: '흔한 제목',
-  name: '그저 이름',
-  like: 123,
-  rank: 5
-}, {
-  photoID: 6,
-  photo: 'abc',
-  head: '흔한 제목',
-  name: '그저 이름',
-  like: 123,
-  rank: 6
-}
-]
+export const Data: RankProps[] = []
