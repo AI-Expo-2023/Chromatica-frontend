9c8ba2c138666860584b01d8feb74b73d15cc5e8
김상구 <spair0039@gmail.com>
2023-04-02 23:20:07 +0900

    ✨: 페이지네이션 테스트

--- pages/my/hearted.tsx
+++ pages/my/hearted.tsx
@@ -1,13 +1,18 @@
 import { PostLister } from "@/components/my/hearted/PostLister";
 import { Title } from "./style";
+import Pagination from "@/components/common/pagination/pagination";
+import { useState } from "react";
 
 const MyHearted = ()=>{
+    const [pageNum,setPageNum] = useState<number>(1);
+
     return(
         <>
             <Title>
                 좋아요 누른 작품
             </Title>
             <PostLister />
+            <Pagination value={pageNum} change={setPageNum} end={30}/>
         </>
     )
 }
