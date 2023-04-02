e8d85a4fddcf799f683952eca438e74693e67093
김상구 <spair0039@gmail.com>
2023-04-02 22:11:10 +0900

    ✨: 기본(대충) 레이아웃

---
+++ components/my/hearted/PostLister.tsx
@@ -0,0 +1,35 @@
+import RankCard from "@/components/common/RankCard/RankCard"
+import styled from "@emotion/styled";
+
+export const PostLister = ()=>{
+    const photo = '';
+    return(
+        <SixHorizon>
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+            <RankCard head="티모대위 출동" like={32} name="구른다슈레기물안경" photo="asdf" rank={3} />
+        </SixHorizon>
+    )
+}
+
+const SixHorizon = styled.div`
+    width: 1300px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+`
