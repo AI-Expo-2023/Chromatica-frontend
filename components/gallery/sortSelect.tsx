86d942d991813fdce466d52e89c876b6279230d4
김상구 <spair0039@gmail.com>
2023-04-05 08:40:53 +0900

    💄: 스타일링

--- components/gallery/sortSelect.tsx
+++ components/gallery/sortSelect.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Button } from "../common/button/style";
 
 interface propsType{
@@ -7,9 +8,16 @@ interface propsType{
 
 export const SortSelecter = ({sort, setSort}:propsType)=>{
     return(
-        <>
+        <HzDiv>
             <Button Gray25>최신순</Button>
             <Button Gray5>인기순</Button>
-        </>
+        </HzDiv>
     )
 }
+
+const HzDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    padding: 20px 0;
+`
