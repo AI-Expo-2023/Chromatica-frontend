2dda004427a2a3ceafc0f0837141dd7726868920
김상구 <spair0039@gmail.com>
2023-04-04 15:16:45 +0900

    ✨: 임시 구현(임시데이터)

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,10 +1,28 @@
+import { PostListerWithSort } from "@/components/gallery/postlister";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function Gallery() {
+    const dta = [
+        {
+            head:'teemodawee',
+            user:'vayne',
+            like: 99,
+            photo: 'as2df',
+        },
+        {
+            head:'teemdodawee',
+            user:'vayne',
+            like: 99,
+            photo: 'asdf',
+        },
+]//임시데이터
+
     const Router = useRouter();
-    const [Sort, setSort] = useState<string>('new');
+    const [Sort, setSort] = useState<string>(Router.query.sort);
     return(
-        <p>{Router.query.sort}</p>
+        <>
+            <PostListerWithSort sort={Sort} data={dta}/>
+        </>
     );
 }
