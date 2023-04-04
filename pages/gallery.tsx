5ebe6d4cd0ae8835e5a05b394faaa66a76973dbb
김상구 <spair0039@gmail.com>
2023-04-04 15:07:14 +0900

    ✨: sort 상태

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Gallery() {
     const Router = useRouter();
+    const [Sort, setSort] = useState<string>('new');
     return(
         <p>{Router.query.sort}</p>
     );
