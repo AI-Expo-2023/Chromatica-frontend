1d06682662b8c223d57eb3dbfc9c7eb9c827da80
김상구 <spair0039@gmail.com>
2023-04-05 15:36:35 +0900

    💄: 여백 및 중앙정렬

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,10 +1,12 @@
 import Pagination from "@/components/common/pagination/pagination";
 import { PostListerWithSort } from "@/components/gallery/postListerSortable";
 import { SortSelecter } from "@/components/gallery/sortSelect";
+import styled from "@emotion/styled";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Gallery() {
+    
     const {query, isReady} = useRouter();
     const [Sort, setSort] = useState<string>(query.sort as string);
     const [Page, setPage] = useState<number>(1);
@@ -15,10 +17,26 @@ export default function Gallery() {
     },[isReady])
 
     return(
-        <>
-            <SortSelecter sort={Sort} setSort={setSort} />
-            <PostListerWithSort sort={Sort==='new' || Sort === 'popular' ? Sort : 'new'} data={dta}/>
-            {Sort==='new' ? <Pagination value={Page} change={setPage} /> : null}
-        </>
+        <CenterContainer>
+            <PaddingContainer>
+                <SortSelecter sort={Sort} setSort={setSort} />
+                <PostListerWithSort sort={Sort==='new' || Sort === 'popular' ? Sort : 'new'} data={}}/>
+                {Sort==='new' ? <Pagination value={Page} change={setPage} /> : null}
+            </PaddingContainer>
+        </CenterContainer>
     );
 }
+
+const PaddingContainer = styled.div`
+    width: 1300px;
+    display: flex;
+    gap: 12px;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 20px;
+`
+
+const CenterContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
