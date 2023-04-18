ce66a6ed06bd83526b3daebcd83aa639d3d305dc
김상구 <spair0039@gmail.com>
2023-04-18 18:29:45 +0900

    💄: 여백 및 중앙정렬

--- pages/my/hearted.tsx
+++ pages/my/hearted.tsx
@@ -2,6 +2,7 @@ import { PostLister } from "@/components/my/hearted/PostLister";
 import { Title } from "./style";
 import Pagination from "@/components/common/pagination/pagination";
 import { useState } from "react";
+import styled from "@emotion/styled";
 
 const MyHearted = ()=>{
     const [pageNum,setPageNum] = useState<number>(1);
@@ -28,14 +29,30 @@ const MyHearted = ()=>{
     ]//일해라 서버
 
     return(
-        <>
-            <Title>
-                좋아요 누른 작품
-            </Title>
-            <PostLister data={ddtt} pageNum={pageNum}/>
-            <Pagination value={pageNum} change={setPageNum} end={Math.ceil(ddtt.length/18)}/>
-        </>
+        <CenterContainer>
+            <PaddingContainer>
+                <Title>
+                    좋아요 누른 작품
+                </Title>
+                <PostLister data={ddtt} pageNum={pageNum}/>
+                <Pagination value={pageNum} change={setPageNum} end={Math.ceil(ddtt.length/18)}/>
+            </PaddingContainer>
+        </CenterContainer>
     )
 }
+
+const PaddingContainer = styled.div`
+    width: 1300px;
+    display: flex;
+    gap: 12px;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 20px;
+`
+
+const CenterContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 export default MyHearted;
