1e17a52f8d9c22230348654e9e18acbdf6479aba
김상구 <spair0039@gmail.com>
2023-04-21 13:19:16 +0900

    🚚: '내가 쓴 글' 전용 postlister +

---
+++ components/my/post/PostLister.tsx
@@ -0,0 +1,27 @@
+import RankCard from "@/components/common/RankCard/RankCard"
+import styled from "@emotion/styled";
+
+export const PostLister4mypost = ({data}:any)=>{
+    return(
+        <SixHorizon>
+            {data
+                .map((aa)=>(
+                    <RankCard
+                        photoID={aa.Photo.photoID}
+                        photo={aa.Photo.photo}
+                        head={aa.Photo.head}
+                        user={aa.User}
+                        like={aa.Photo.like}
+                        key={aa.photoID}
+                    />
+            ))}
+        </SixHorizon>
+    )
+}
+
+const SixHorizon = styled.div`
+    max-width: 1300px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+`
