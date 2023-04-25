6534c1ed24da2d484b3c0ab4e41e4cd9a794ff5a
김상구 <spair0039@gmail.com>
2023-04-25 09:13:54 +0900

    ✨: 정렬선택기 대응

--- components/gallery/sortSelect.tsx
+++ components/gallery/sortSelect.tsx
@@ -3,7 +3,7 @@ import { Button } from "../common/button/style";
 
 interface propsType{
     sort: string;
-    setSort: React.Dispatch<React.SetStateAction<string>>;
+    setSort: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const SortSelecter = ({sort, setSort}:propsType)=>{
@@ -11,14 +11,14 @@ export const SortSelecter = ({sort, setSort}:propsType)=>{
         return(
             <HzDiv>
                 <Button Gray25>최신순</Button>
-                <Button Gray5 onClick={()=>setSort('popular')}>인기순</Button>
+                <Button Gray5 onClick={()=>setSort(true)}>인기순</Button>
             </HzDiv>
         )
     }
     else{
         return(
             <HzDiv>
-                <Button Gray5 onClick={()=>setSort('new')}>최신순</Button>
+                <Button Gray5 onClick={()=>setSort(false)}>최신순</Button>
                 <Button Gray25>인기순</Button>
             </HzDiv>
         )
