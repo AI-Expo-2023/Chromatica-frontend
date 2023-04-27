e7ba769004dc24664fe7e5a11d355ffa2746b89b
kimgyuha <kimgyuha25@gmail.com>
2023-04-27 21:40:37 +0900

    :recycle: :: colorPalette map돌리기

--- components/img2img/colorPalette.tsx
+++ components/img2img/colorPalette.tsx
@@ -6,15 +6,15 @@ const ColorPalette = ({ settingOptions, setSettingOptions }: CanvasUseState): JS
   const onClick = (data: string) => {
     setSettingOptions({ ...settingOptions, color: data, tool: true, paint: false })
   }
+  const color = ["#404040", "#DA5858", "#ED9454", "#FADD75", "#3FC661", "#497EE9", "#9747FF"];
+
   return (
     <SettingContainer>
-      <SimpleColor onClick={() => onClick("#404040")} color="#404040" />
-      <SimpleColor onClick={() => onClick("#DA5858")} color="#DA5858" />
-      <SimpleColor onClick={() => onClick("#ED9454")} color="#ED9454" />
-      <SimpleColor onClick={() => onClick("#FADD75")} color="#FADD75" />
-      <SimpleColor onClick={() => onClick("#3FC661")} color="#3FC661" />
-      <SimpleColor onClick={() => onClick("#497EE9")} color="#497EE9" />
-      <SimpleColor onClick={() => onClick("#9747FF")} color="#9747FF" />
+      {
+        color.map((e) => {
+          return <SimpleColor onClick={() => onClick(e)} color={e} />
+        })
+      }
     </SettingContainer>
   )
 }
@@ -37,7 +37,7 @@ const SimpleColor = styled.button<{ color: string }>`
   border-radius: 50%;
   transition: 0.1s;
   border: none;
-
+  background-color: red;
   background-color: ${props => props.color};
   &:hover{
     scale: 1.1;
